613e342cf5cce5175b1b66003754909a107df640
gitlab-runner <do-not-reply@cypress.com>
2025-09-24 06:44:45 -0500

    Upload mtb-example-psoc-edge-voice-assistant-deploy [3998]

---
+++ proj_cm33_ns/main.c
@@ -0,0 +1,106 @@
+/****************************************************************************
+* File Name        : main.c
+*
+* Description      : This source file contains the main routine for CM33 CPU
+*
+* Related Document : See README.md
+*
+********************************************************************************
+* Copyright 2025, Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+*
+* This software, including source code, documentation and related
+* materials ("Software") is owned by Cypress Semiconductor Corporation
+* or one of its affiliates ("Cypress") and is protected by and subject to
+* worldwide patent protection (United States and foreign),
+* United States copyright laws and international treaty provisions.
+* Therefore, you may use this Software only as provided in the license
+* agreement accompanying the software package from which you
+* obtained this Software ("EULA").
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software
+* source code solely for use in connection with Cypress's
+* integrated circuit products.  Any reproduction, modification, translation,
+* compilation, or representation of this Software except as specified
+* above is prohibited without the express written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer
+* of such system or application assumes all risk of such use and in doing
+* so agrees to indemnify Cypress against all liability.
+*******************************************************************************/
+
+/*******************************************************************************
+* Header Files
+*******************************************************************************/
+
+#include "cybsp.h"
+
+/*****************************************************************************
+ * Macros
+ *****************************************************************************/
+/* The timeout value in microsecond used to wait for core to be booted
+    Use value 0U for infinite wait till the core is booted successfully.*/
+#define CM55_BOOT_WAIT_TIME_USEC            (10U)
+/* App boot address for CM55 project */
+#define CM55_APP_BOOT_ADDR          (CYMEM_CM33_0_m55_nvm_START + \
+                                        CYBSP_MCUBOOT_HEADER_SIZE)
+                                        
+/*****************************************************************************
+ * Function Prototypes
+ *****************************************************************************/
+
+
+/*****************************************************************************
+ * Function Name: main
+ *****************************************************************************
+ * Summary:
+ * This is the main function for CM33 non-secure application. 
+ *    1. It initializes the device and board peripherals.
+ *    2. It enables the  CM55 CPU using 'Cy_SysEnableCM55'
+ *    3. Go to deep-sleep
+ *
+ * Parameters:
+ *  void
+ *
+ * Return:
+ *  int
+ *
+ *****************************************************************************/
+int main(void)
+{
+    cy_rslt_t result;
+
+    /* Initialize the device and board peripherals */
+    result = cybsp_init();
+
+    /* Board init failed. Stop program execution */
+    if (CY_RSLT_SUCCESS != result)
+    {
+        CY_ASSERT(0);
+    }
+
+    /* Enable global interrupts */
+    __enable_irq();
+
+    /* Enable CM55. */
+    /* CM55_APP_BOOT_ADDR must be updated if CM55 memory layout is changed.*/
+    Cy_SysEnableCM55(MXCM55, CM55_APP_BOOT_ADDR, CM55_BOOT_WAIT_TIME_USEC);
+
+    while (1)
+    {
+        Cy_SysPm_CpuEnterDeepSleep(CY_SYSPM_WAIT_FOR_INTERRUPT);
+    }
+
+    return 0;
+}
+
+/* [] END OF FILE */
